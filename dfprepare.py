532a4f2de1a47d88af60fa4e6f2c2677010ae7bf
Vadim Shaidurov <shaidurovvadik@yandex.ru>
2023-12-20 23:27:37 +0300

    Fifth enhanced commit

--- dfprepare.py
+++ dfprepare.py
@@ -1,16 +1,21 @@
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
 
 
-def norm_data(df, pass_columns=()):
+def norm_data(df, ignore_columns=()):
     """ Нормализуем данные """
+    df = df.copy()
+
+    label_encoder = LabelEncoder()
+    scaler = MinMaxScaler()
     for col in df.columns:
-        label_encoder = LabelEncoder()
-        scaler = MinMaxScaler()
-        if df[col].dtype.kind in 'O':
+        df_kind = df[col].dtype.kind
+        if col in ignore_columns:
+            pass
+        elif df_kind in 'b':
+            df[col] = df[col].replace({True: 1, False: 0})
+        elif df_kind in 'O':
             df[col] = label_encoder.fit_transform(df[col])
-        elif df[col].dtype.kind in 'iufc':
-            if (df[col].min() != 0 and df[col].max() != 1) and (col not in pass_columns):
-                df[col] = scaler.fit_transform(df[[col]])
-            else:
-                pass
+        elif df_kind in 'iufc':
+            df[col] = scaler.fit_transform(df[[col]])
+
     return df
